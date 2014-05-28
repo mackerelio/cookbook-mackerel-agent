50c7e1de517b85047dc08dedf91a1214fa904946
y_uuki <yuki.tsubo@gmail.com>
2014-05-29 00:59:13 +0900

    Add roles node attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,3 +2,4 @@ default['mackerel-agent']['apikey']  = nil
 default['mackerel-agent']['pidfile'] = nil
 default['mackerel-agent']['root'] = nil
 default['mackerel-agent']['verbose'] = nil
+default['mackerel-agent']['roles'] = nil    # eg. ["My-Service:app", "Another-Service:db"]
