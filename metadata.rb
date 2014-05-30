c03a9c712cbc9a94533f827d37a5a695a14e67f4
y_uuki <yuki.tsubo@gmail.com>
2014-05-30 16:11:05 +0900

    Fix yum version < 3.0

--- metadata.rb
+++ metadata.rb
@@ -7,7 +7,7 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
 depends 'apt'
-depends 'yum', '>= 3.0'
+depends 'yum', '< 3.0'
 
 %w(debian ubuntu redhat centos amazon).each do |os|
   supports os
