de43e9e53310c2ffef1e9e8a7a371c6654cc9b0d
y_uuki <yuki.tsubo@gmail.com>
2014-09-12 13:58:41 +0900

    Fix email address

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'mackerel-agent'
 maintainer       'Hatena Co., Ltd.'
-maintainer_email 'admin@mackerel.io'
+maintainer_email 'support@mackerel.io'
 license          'Apache License, Version 2.0'
 description      'Installs/Configures mackerel-agent'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
