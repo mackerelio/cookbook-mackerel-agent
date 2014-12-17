b8c2f9f5c2a13548ba16949eed9f5f6e536c8cee
y_uuki <yuki.tsubo@gmail.com>
2014-12-17 14:07:51 +0900

    Create specific recipe for mackerel-agent-plugins instead of the node attribute

---
+++ recipes/plugins.rb
@@ -0,0 +1,12 @@
+#
+# Cookbook Name:: mackerel-agent
+# Recipe:: plugins
+#
+# Copyright 2014, Hatena Co., Ltd.
+#
+# Apache License, Version 2.0
+#
+
+package "mackerel-agent-plugins" do
+  action :install
+end
