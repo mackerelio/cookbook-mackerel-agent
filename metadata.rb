9d9de0c805298549ce371b0c80f75bfcae262cb6
y_uuki <yuki.tsubo@gmail.com>
2015-01-04 08:42:56 +0900

    Change maintainer email

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'mackerel-agent'
 maintainer       'Hatena Co., Ltd.'
-maintainer_email 'support@mackerel.io'
+maintainer_email 'mackerel-developers@hatena.ne.jp'
 license          'Apache License, Version 2.0'
 description      'Installs/Configures mackerel-agent'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
