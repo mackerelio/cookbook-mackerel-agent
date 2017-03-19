5c4dd99eb4622696f26891ed5fca9663c10a5224
hagihala <hagihala@hatena.ne.jp>
2017-03-20 03:59:59 +0900

    Remove workaround for CircleCI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,11 +4,3 @@ set :backend, :docker
 
 set :docker_url, ENV['DOCKER_HOST']
 set :docker_image, ENV['DOCKER_IMAGE']
-
-# monkey-patch to prevent specinfra from trying to delete container
-# https://circleci.com/docs/1.0/docker-btrfs-error/
-class Specinfra::Backend::Docker
-  def cleanup_container
-    @container.stop
-  end
-end
